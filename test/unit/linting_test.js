a53fba800b1b1a0f898c51435b58ae29843d8656
Thomas Huang <thomas.b.huang@gmail.com>
2017-06-15 11:53:08 -0700

    Remove linting for unit testing while running tests

--- test/unit/linting_test.js
+++ test/unit/linting_test.js
@@ -1,9 +1,9 @@
 // LINTING:
-const lint = require('mocha-eslint')
-const lintPaths = ['app/**/*.js', 'ui/**/*.js', 'test/**/*.js', '!node_modules/**', '!dist/**', '!docs/**', '!app/scripts/chromereload.js']
+// const lint = require('mocha-eslint')
+// const lintPaths = ['app/**/*.js', 'ui/**/*.js', 'test/**/*.js', '!node_modules/**', '!dist/**', '!docs/**', '!app/scripts/chromereload.js']
 
-const lintOptions = {
-  strict: false,
-}
+// const lintOptions = {
+//   strict: false,
+// }
 
-lint(lintPaths, lintOptions)
+// lint(lintPaths, lintOptions)
